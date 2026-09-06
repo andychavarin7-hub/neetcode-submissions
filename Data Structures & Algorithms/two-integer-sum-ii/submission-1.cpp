d54fe343e869a31e5c42bb59e971bc262ab7fe4a
andychavarin7-hub <andy.chavarin7@gmail.com>
2026-09-06 11:27:32 -0700

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+        /* values are in increasing order, no add space allowed */
+        /* our constraints impose numbers has at least 2 elems */
+
+        int left =0;
+        int right = numbers.size() - 1;
+
+        while(left < right){
+
+            int diff = numbers[left] + numbers[right] - target;
+
+            if(diff > 0) { 
+                --right;  
+            } else if (diff < 0){
+                ++left;
+            } else {
+                /*we found the values*/
+                return {left + 1,right + 1};
+            }
+
+        }
+
+        return {0,0};
+    }
+};
