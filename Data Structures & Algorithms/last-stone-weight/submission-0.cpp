ff1a17f61b28dc5f0756022a6a49dd2a49c7ddda
andychavarin7-hub <andy.chavarin7@gmail.com>
2026-09-07 18:22:53 -0700

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        std::priority_queue<int,std::vector<int>,std::less<int>> stoneQueue;
+
+        for(auto s:stones){
+            stoneQueue.push(s);
+        }
+        int h1 = 0;
+        int h2 = 0;
+        while(stoneQueue.size() >=2){
+
+            h1 = stoneQueue.top();
+            stoneQueue.pop();
+            
+            h2 = stoneQueue.top();
+            stoneQueue.pop();
+
+            if(h1 == h2){
+                continue;
+            } else {
+                int nweight = (h1>h2)? h1-h2: h2-h1;
+                stoneQueue.push(nweight);
+            }
+
+
+        }
+
+        return stoneQueue.empty()? 0: stoneQueue.top();
+    }
+};
