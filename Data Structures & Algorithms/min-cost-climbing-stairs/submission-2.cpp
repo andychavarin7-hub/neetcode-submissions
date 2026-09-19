bfb76baa3bf749ea24ddbfca71952a44b62f5413
andychavarin7-hub <andy.chavarin7@gmail.com>
2026-09-19 16:41:14 -0700

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        const int sz = cost.size();
+        std::vector<int> &cache = cost;
+        for(auto ii=2; ii < cost.size(); ++ii){
+            cache[ii] = cost[ii] + std::min(cache[ii-2], cache[ii-1]);
+              
+        }
+        /* because of our constriants we're guarnteed that cost is at minimum size 2*/
+
+        int minCost =0;
+        if(sz > 1) {
+            minCost = std::min( cache[sz-1], cache[sz-2]);
+        } else if (sz > 0){
+            minCost = cache[sz-1];
+        } else {
+            /*do nothing */
+        }
+
+        return minCost;
+    }
+};
