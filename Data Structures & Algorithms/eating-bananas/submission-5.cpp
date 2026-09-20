cfefe0c03d013f6256f7967e7ff482b899a6cabb
andychavarin7-hub <andy.chavarin7@gmail.com>
2026-09-20 11:26:57 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int upperbnd = 0;
+        for(auto elem: piles){
+            upperbnd = std::max(upperbnd,elem);
+        }
+
+        std::cout << "max k rate is : " << upperbnd << std::endl;
+
+        int lowerbnd = 1;
+        int midbnd =0;
+        int cnt = 0;
+        int res =0;
+        while( lowerbnd<= upperbnd){
+            midbnd =  (upperbnd + lowerbnd)/2;
+            //std::cout << "neew k rate is : " << midbnd;
+            long long time =0;
+            for(auto elem:piles){
+                time += (elem + midbnd - 1)/midbnd; //round up
+            }
+
+            if(time <= h){
+                /*tooo fast slow down */\
+                res = midbnd;
+                upperbnd = midbnd - 1;
+                //std::cout << ", too fast" << std::endl;
+            } else if (time > h){
+                /*not fast enough */
+                lowerbnd  = midbnd + 1;
+                //std::cout << ", too slow" << std::endl;
+            } 
+        }
+
+        return res;    
+    }
+
+};
